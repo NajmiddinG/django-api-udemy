c98be8a3977b3cd36bd1209837e1b086d8885ea3
NajmiddinG <najmiddindev@gmail.com>
2022-10-11 17:12:52 +0500

    fixing postgres

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,8 @@ def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,6 +3,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     
     def test_add_numbers(self):
