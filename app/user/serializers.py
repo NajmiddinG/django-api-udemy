fe7e486eb399bf84553a9f99891de783fac279b1
NajmiddinG <najmiddingiyosiddinov@Najmiddins-MacBook-Pro.local>
2022-10-24 17:20:49 +0500

    12 3

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,8 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}
-        }
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, valudated_data):
         return get_user_model().objects.create_user(**valudated_data)
