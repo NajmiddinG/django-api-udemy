d1aa06734d8337c4b2f84ba27c16f435b54b43fc
NajmiddinG <najmiddingiyosiddinov@Najmiddins-MacBook-Pro.local>
2022-10-24 17:19:49 +0500

    12 3

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', spec_api.as_view(), name='api-schema'),
     path('api/docs/',
-        spec_swag.as_view(url_name='api-schema'), name='api-docs'),
+                     spec_swag.as_view(url_name='api-schema'), name='api-docs'),
     path('api/user/', include('user.urls'))
 ]
