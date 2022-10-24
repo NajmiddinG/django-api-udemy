de06eb7cde7a74e224a8da654fee66731df96f17
NajmiddinG <najmiddingiyosiddinov@Najmiddins-MacBook-Pro.local>
2022-10-24 17:28:46 +0500

    12 4

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import \
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', spec_api.as_view(), name='api-schema'),
-    path('api/docs/',
-                     spec_swag.as_view(url_name='api-schema'), name='api-docs'),
+    path('api/docs/', spec_swag.as_view(
+        url_name='api-schema'), name='api-docs'),
     path('api/user/', include('user.urls'))
 ]
