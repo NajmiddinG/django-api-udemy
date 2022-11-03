c244e9030bf222931dde28d6bea0757c488d102d
NajmiddinG <najmiddingiyosiddinov@Najmiddins-MacBook-Pro.local>
2022-11-03 20:13:30 +0500

    110. Write tests for deleting ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -37,6 +38,19 @@ class TagViewSet(mixins.DestroyModelMixin,
                  viewsets.GenericViewSet):
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
