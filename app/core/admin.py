1afbd599e13a5f2785981e9fdbe30c6ef1dd85f8
NajmiddinG <najmiddingiyosiddinov@Najmiddins-MacBook-Pro.local>
2022-11-16 15:22:07 +0500

    wer

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,6 +14,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -38,7 +39,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
